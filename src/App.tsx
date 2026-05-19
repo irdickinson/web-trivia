987dbb157505ba15a1ef7c020f0b291ab62f11ce
irdickinson <isaacrdickinson@gmail.com>
2026-05-19 13:44:16 -0500

    perf(app): lazy-load all routes to reduce initial bundle size

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,30 @@
+import { lazy, Suspense } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext'
 import { RequireAuth, RedirectIfAuth } from './components/auth/RequireAuth'
-import Home from './pages/Home'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import UpgradeAccount from './pages/UpgradeAccount'
-import Lobby from './pages/Lobby'
-import Room from './pages/Room'
+import { LoadingScreen } from './components/ui/LoadingScreen'
+
+const Home = lazy(() => import('./pages/Home'))
+const SignIn = lazy(() => import('./pages/SignIn'))
+const SignUp = lazy(() => import('./pages/SignUp'))
+const UpgradeAccount = lazy(() => import('./pages/UpgradeAccount'))
+const Lobby = lazy(() => import('./pages/Lobby'))
+const Room = lazy(() => import('./pages/Room'))
 
 export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/sign-in"
-            element={<RedirectIfAuth><SignIn /></RedirectIfAuth>}
-          />
-          <Route
-            path="/sign-up"
-            element={<RedirectIfAuth><SignUp /></RedirectIfAuth>}
-          />
-          <Route
-            path="/upgrade"
-            element={<RequireAuth anonymousOnly><UpgradeAccount /></RequireAuth>}
-          />
-          <Route
-            path="/lobby"
-            element={<RequireAuth><Lobby /></RequireAuth>}
-          />
-          <Route
-            path="/room/:code"
-            element={<RequireAuth><Room /></RequireAuth>}
-          />
-        </Routes>
+        <Suspense fallback={<LoadingScreen />}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sign-in" element={<RedirectIfAuth><SignIn /></RedirectIfAuth>} />
+            <Route path="/sign-up" element={<RedirectIfAuth><SignUp /></RedirectIfAuth>} />
+            <Route path="/upgrade" element={<RequireAuth anonymousOnly><UpgradeAccount /></RequireAuth>} />
+            <Route path="/lobby" element={<RequireAuth><Lobby /></RequireAuth>} />
+            <Route path="/room/:code" element={<RequireAuth><Room /></RequireAuth>} />
+          </Routes>
+        </Suspense>
       </AuthProvider>
     </BrowserRouter>
   )
